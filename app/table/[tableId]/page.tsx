c40336e50a78cb779d25d43a089e451faf2381b1
MJenius <mjenius1357@gmail.com>
2026-05-27 21:22:50 +0530

    fix: Add T1 to generateStaticParams for demo table
    - Include T1 in the list of static table IDs
    - Fixes 404 error on /table/T1 route
    - Generates static HTML for main demo table

--- app/table/[tableId]/page.tsx
+++ app/table/[tableId]/page.tsx
@@ -3,6 +3,7 @@ import { SmartDiningApp } from "@/components/SmartDiningApp";
 export async function generateStaticParams() {
   // Generate static params for sample table IDs for static export
   return [
+    { tableId: "T1" },
     { tableId: "table-1" },
     { tableId: "table-2" },
     { tableId: "table-3" },
